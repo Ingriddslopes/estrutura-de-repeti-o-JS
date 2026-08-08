683072d03e712af964aba878ad4398d31c47c6ad
Ingrid Silva <ingriddsl08@gmail.com>
2026-08-08 13:44:50 -0300

    projeto concluido

--- main.js
+++ main.js
@@ -5,16 +5,22 @@ const botaoCriar =document.getElementById('criar')
 function criarCaixa(numero) {
     const caixaConteiner=document.createElement('div')
     const caixaNumero=document.createElement('div')
-   
-    caixaNumero.className='caixa'
+    
+    caixaNumero.className='caixa-numero'
     caixaNumero.textContent= numero
 
-    caixaConteiner.append(caixaNumero)
+    const caixaNome=document.createElement('div')
+    caixaNome.className= 'caixa-nome'
+    caixaNome.textContent= 'Aluno -'+ numero
+
+    caixaConteiner.className='caixa-numero'
+    caixaConteiner.append(caixaNumero,caixaNome)
     return caixaConteiner
 }
 
 function adicionarCaixa(){
     const conteiner=document.getElementById('conteiner')
+   
 
    for(let i=1; i<=1000;i++){
     const caixa= criarCaixa(i)
